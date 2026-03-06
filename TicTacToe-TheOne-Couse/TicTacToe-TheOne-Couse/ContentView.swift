ce85d4f3eecbd3928477df75058a08d42666bad9
eijirou <sacjapan2009@gmail.com>
2026-03-06 13:05:54 +0900

    Create Xcode Project

---
+++ TicTacToe-TheOne-Couse/TicTacToe-TheOne-Couse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe-TheOne-Couse
+//
+//  Created by 野澤英二郎 on 2026/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToe-TheOne-Couse/TicTacToe-TheOne-Couse/TicTacToe_TheOne_CouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_TheOne_CouseApp.swift
+//  TicTacToe-TheOne-Couse
+//
+//  Created by 野澤英二郎 on 2026/03/06.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_TheOne_CouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
